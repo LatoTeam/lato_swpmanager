28c45390ec439c3b343ac2c6801081236865471b
Gregorio Galante <gregogalante@gmail.com>
2016-11-15 11:30:02 +0100

    auto-commit

--- app/assets/javascripts/lato_swpmanager/application.js
+++ app/assets/javascripts/lato_swpmanager/application.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
 
   $('.lato-swpmanager-task-start-date-input').change(function() {
     $('.lato-swpmanager-task-end-date-input').val($(this).val())
+    $( "input[name='task[end_date]']" ).val($(this).val())
   })
 
 })
